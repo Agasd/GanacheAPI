3bbc9cdc31f7bbf6f91daa2c4143bba5e796b32c
Aga <blaise.agardi@gmail.com>
2019-10-16 21:58:35 +0200

    HTTPS redirect disabled

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace GanacheAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
