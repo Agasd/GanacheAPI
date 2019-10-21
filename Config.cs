11ca9b5a8d2b3b3802d03854a3f94940ebac4657
Aga <blaise.agardi@gmail.com>
2019-10-21 22:22:46 +0200

    Basic API endpoints are configured and tested

---
+++ Config.cs
@@ -0,0 +1,13 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GanacheAPI
+{
+    public class Config
+    {
+        public static readonly String GANACHE = "HTTP://127.0.0.1:7545";
+    }
+}
---
+++ Controllers/WalletsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nethereum.Web3;
+using GanacheAPI.Models;
+
+
+namespace GanacheAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class WalletsController : Controller
+    {
+
+        /*
+         [GET] ether balance using wallet's public key
+          /api/wallets/public_key
+         */
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return Wallet.getAccountBalance(id).Result;
+        }
+
+
+        /*
+         * [POST]
+         * Create wallet: /api/wallets
+         */
+        [HttpPost]
+
+        public string Post()
+        {
+
+            return Wallet.CreateNewEthereumAddressAsync();
+        }
+
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        public void Put([FromBody]Transaction transaction)
+        {
+            transaction.executeTransaction();
+        }
+
+    }
+}
