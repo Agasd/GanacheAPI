c5d10ee11e0902fd35b70db819c9624e16e659bf
Aga <asd>
2019-11-18 21:04:02 +0100

    Database stuff

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GanacheAPI.Models
+{
+    [Table("Users")]
+
+    public class User
+    {
+        [Key]
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public int credit { get; set; }
+        public int user_type { get; set; }
+    }
+}
